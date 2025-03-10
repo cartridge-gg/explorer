fc04f98fa540d65992c0bf354925ba5e32f6b850
Ammar Arif <evergreenkary@gmail.com>
2025-03-10 14:12:29 -0400

    feat: default font family `Space Mono`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Mono', 'monospace'],
+      },
+      fontSize: {
+        'xs': '9px',
+        'sm': '11px',
+        'base': '11px',
+        'md': '13px',
+        'lg': '13px',
+      },
+      borderRadius: {
+        'sm': '3px',
+        'DEFAULT': '4px',
+        'md': '4px',
+        'lg': '6px',
+      },
+    },
   },
   plugins: [],
 };
