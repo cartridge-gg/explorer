c293e80e983955321e924ba7ade35662f97096ef
Ammar Arif <evergreenkary@gmail.com>
2025-03-12 11:40:26 -0400

    feat: remove logo

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -11,13 +11,6 @@ export default function Header() {
   }, [navigate]);
   return (
     <div className="mb-8">
-      <div
-        onClick={handleNavigation}
-        className="w-fit border-l-4 border-[#4A4A4A] cursor-pointer mb-4"
-      >
-        <h1 className=" px-2">explrr .</h1>
-      </div>
-
       <div className="max-w-[467px]">
         <SearchBar />
       </div>
