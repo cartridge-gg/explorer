572e5e749c25670332888cfb759fd4d55924c95a
Ammar Arif <evergreenkary@gmail.com>
2025-03-24 15:47:00 -0400

    refactor: update master container size

--- src/modules/TransactionDetails/page.tsx
+++ src/modules/TransactionDetails/page.tsx
@@ -560,8 +560,8 @@ export default function TransactionDetails() {
         }
       />
 
-      <div className="flex flex-col sl:flex-row sl:h-[66vh] gap-4">
-        <div className="flex w-full sl:w-[35%] sl:min-w-[35%] sl:max-w-[35%] flex-col gap-[6px] sl:overflow-y-auto">
+      <div className="flex flex-col sl:flex-row sl:h-[70vh] gap-4">
+        <div className="sl:w-[468px] min-w-[468px] flex flex-col gap-[6px] sl:overflow-y-scroll">
           <SectionBox variant="upper-half">
             <SectionBoxEntry title="Hash">
               {isMobile
