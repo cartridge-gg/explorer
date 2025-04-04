057640d7333bc1c92d42ece8bd7554652dbbc4f4
ayushtom <tomar2585@gmail.com>
2025-04-03 23:38:15 -0700

    feat: add custom rpc config

--- src/constants/rpc.ts
+++ src/constants/rpc.ts
@@ -1,3 +1,5 @@
+import { shortString } from "starknet";
+
 export const EXECUTION_RESOURCES_KEY_MAP = {
   bitwise_builtin_applications: "bitwise",
   pedersen_builtin_applications: "pedersen",
@@ -14,11 +16,26 @@ export const EXECUTION_RESOURCES_KEY_MAP = {
 export const IS_EMBEDDED =
   window.IS_EMBEDDED ?? import.meta.env.VITE_IS_EMBEDDED;
 
-export const RPC_URL = IS_EMBEDDED
-  ? window.location.origin
-  : window.RPC_URL ?? import.meta.env.VITE_RPC_URL;
+export const RPC_URL =
+  IS_EMBEDDED === "true"
+    ? window.location.origin
+    : window.RPC_URL ?? import.meta.env.VITE_RPC_URL;
 
-export const CHAIN_ID = window.CHAIN_ID ?? import.meta.env.VITE_CHAIN_ID;
+export const CHAIN_ID =
+  IS_EMBEDDED === "true"
+    ? window.CHAIN_ID
+    : import.meta.env.VITE_CHAIN_ID ??
+      BigInt(shortString.encodeShortString("KATANA"));
+
+export const CHAIN_NAME =
+  IS_EMBEDDED === "true"
+    ? window.CHAIN_NAME
+    : import.meta.env.VITE_CHAIN_NAME ?? "Katana Local";
+
+export const CHAIN_NETWORK =
+  IS_EMBEDDED === "true"
+    ? window.CHAIN_NETWORK
+    : import.meta.env.VITE_CHAIN_NETWORK ?? "katana";
 
 // Cache constants
 export const CACHE_TIME = 1000 * 60 * 60 * 24; // 24 hours
--- src/store/starknetProvider.tsx
+++ src/store/starknetProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { sepolia, mainnet } from "@starknet-react/chains";
+import { sepolia, mainnet, Chain } from "@starknet-react/chains";
 import {
   StarknetConfig,
   jsonRpcProvider,
@@ -9,7 +9,13 @@ import {
 import { Connector, InjectedConnector } from "@starknet-react/core";
 import ControllerConnector from "@cartridge/connector/controller";
 import { WebWalletConnector } from "starknetkit/webwallet";
-import { CHAIN_ID, RPC_URL } from "@/constants/rpc";
+import {
+  CHAIN_ID,
+  CHAIN_NETWORK,
+  CHAIN_NAME,
+  RPC_URL,
+  IS_EMBEDDED,
+} from "@/constants/rpc";
 
 const ENABLE_CONTROLLER =
   window.ENABLE_CONTROLLER ?? import.meta.env.VITE_ENABLE_CONTROLLER === "true";
@@ -38,6 +44,28 @@ export const availableConnectors: Connector[] = [
   new WebWalletConnector({ url: "https://web.argent.xyz" }),
 ];
 
+export const katanaLocalChain = {
+  id: CHAIN_ID,
+  network: CHAIN_NETWORK,
+  name: CHAIN_NAME,
+  nativeCurrency: {
+    address:
+      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+
+  rpcUrls: {
+    default: {
+      http: [RPC_URL],
+    },
+    public: {
+      http: [RPC_URL],
+    },
+  },
+} as const satisfies Chain;
+
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
   const providers = jsonRpcProvider({
     rpc: () => ({
@@ -49,7 +77,13 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <StarknetConfig
-      chains={[import.meta.env.VITE_IS_TESTNET === "true" ? sepolia : mainnet]}
+      chains={[
+        IS_EMBEDDED === "true"
+          ? import.meta.env.VITE_IS_TESTNET === "true"
+            ? sepolia
+            : mainnet
+          : katanaLocalChain,
+      ]}
       provider={providers}
       connectors={connectors}
     >
