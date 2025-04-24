74de3f3be5e985b53974d60cab056886a7c59441
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-04-24 10:53:58 +0200

    Fix mobile layout

--- src/modules/ContractDetails/page.tsx
+++ src/modules/ContractDetails/page.tsx
@@ -114,121 +114,113 @@ export default function ContractDetails() {
     useBalances(contractAddress);
 
   return (
-    <>
-      <div className="w-full flex-grow gap-8">
-        <div className="mb-2">
-          <Breadcrumb>
-            <BreadcrumbItem href="/">Explorer</BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem href="/">Contracts</BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage className=" text-sm">
-                {isMobile && contractAddress
-                  ? truncateString(contractAddress)
-                  : contractAddress}
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          </Breadcrumb>
+    <div className="w-full flex-grow gap-8">
+      <div className="mb-2">
+        <Breadcrumb>
+          <BreadcrumbItem href="/">Explorer</BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem href="/">Contracts</BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className=" text-sm">
+              {isMobile && contractAddress
+                ? truncateString(contractAddress)
+                : contractAddress}
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+
+      <PageHeader className="mb-6" title="Contract" />
+
+      <div className="flex flex-col sl:flex-row sl:h-[76vh] gap-4">
+        {/* Contract Info Section */}
+        <div className="sl:w-[468px] flex flex-col gap-[6px] sl:overflow-y-scroll">
+          <SectionBox>
+            <SectionBoxEntry title="Address">
+              {isMobile && contractAddress
+                ? truncateString(contractAddress)
+                : contractAddress}
+            </SectionBoxEntry>
+
+            <SectionBoxEntry title="Class Hash">
+              {isMobile && classHash ? truncateString(classHash) : classHash}
+            </SectionBoxEntry>
+          </SectionBox>
+
+          <SectionBox title="Balances">
+            <table className="w-full">
+              <tbody>
+                <tr>
+                  <th className="text-left w-[53px] bg-white font-bold">
+                    STRK
+                  </th>
+                  <td className="text-left">
+                    {isStrkLoading
+                      ? "0.00"
+                      : balances.strk !== undefined
+                        ? (Number(balances.strk) / 10 ** 18).toString()
+                        : "N/A"}
+                  </td>
+                </tr>
+
+                <tr>
+                  <th className="text-left bg-white font-bold">ETH</th>
+                  <td className="text-left">
+                    {isEthLoading
+                      ? "0.00"
+                      : balances.eth !== undefined
+                        ? (Number(balances.eth) / 10 ** 18).toString()
+                        : "N/A"}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </SectionBox>
         </div>
 
-        <PageHeader className="mb-6" title="Contract" />
+        <div className="h-full flex-grow grid grid-rows-[min-content_1fr]">
+          <DetailsPageSelector
+            selected={DataTabs[0]}
+            onTabSelect={setSelectedDataTab}
+            items={DataTabs.map((tab) => ({
+              name: tab,
+              value: tab,
+            }))}
+          />
 
-        <div className="flex flex-col sl:flex-row sl:h-[76vh] gap-4">
-          {/* Contract Info Section */}
-          <div className="sl:w-[468px] min-w-[468px] flex flex-col gap-[6px] sl:overflow-y-scroll">
-            <SectionBox>
-              <SectionBoxEntry title="Address">
-                {isMobile && contractAddress
-                  ? truncateString(contractAddress)
-                  : contractAddress}
-              </SectionBoxEntry>
+          <div className="bg-white flex flex-col gap-3 mt-[6px] px-[15px] py-[17px] border border-borderGray overflow-auto">
+            <div className="w-full h-full overflow-auto">
+              {(() => {
+                if (!contract) {
+                  return null
+                }
 
-              <SectionBoxEntry title="Class Hash">
-                {isMobile && classHash ? truncateString(classHash) : classHash}
-              </SectionBoxEntry>
-            </SectionBox>
-
-            <SectionBox title="Balances">
-              <table className="w-full">
-                <tbody>
-                  <tr>
-                    <th className="text-left w-[53px] bg-white font-bold">
-                      STRK
-                    </th>
-                    <td className="text-left">
-                      {isStrkLoading
-                        ? "0.00"
-                        : balances.strk !== undefined
-                          ? (Number(balances.strk) / 10 ** 18).toString()
-                          : "N/A"}
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <th className="text-left bg-white font-bold">ETH</th>
-                    <td className="text-left">
-                      {isEthLoading
-                        ? "0.00"
-                        : balances.eth !== undefined
-                          ? (Number(balances.eth) / 10 ** 18).toString()
-                          : "N/A"}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </SectionBox>
-          </div>
-
-          <div className="h-full flex-grow grid grid-rows-[min-content_1fr]">
-            <DetailsPageSelector
-              selected={DataTabs[0]}
-              onTabSelect={setSelectedDataTab}
-              items={DataTabs.map((tab) => ({
-                name: tab,
-                value: tab,
-              }))}
-            />
-
-            <div className="bg-white flex flex-col gap-3 mt-[6px] px-[15px] py-[17px] border border-borderGray overflow-auto">
-              <div className="w-full h-full overflow-auto">
-                {(() => {
-                  if (!contract) {
-                    return null
-                  }
-
-                  switch (selectedDataTab) {
-                    case "Read Contract":
-                      return <ContractReadInterface
-                        contract={contract}
-                        functions={readFunctions}
-                      />
-                    case "Write Contract":
-                      return <ContractWriteInterface
-                        contract={contract}
-                        functions={writeFunctions}
-                      />
-                    case "Code":
-                      return <Code {...codeProps} />
-                    default:
-                      return (
-                        <div className="h-full p-2 flex items-center justify-center min-h-[150px] text-xs lowercase">
-                          <span className="text-[#D0D0D0]">No data found</span>
-                        </div>
-                      )
-                  }
-                })()}
-              </div>
+                switch (selectedDataTab) {
+                  case "Read Contract":
+                    return <ContractReadInterface
+                      contract={contract}
+                      functions={readFunctions}
+                    />
+                  case "Write Contract":
+                    return <ContractWriteInterface
+                      contract={contract}
+                      functions={writeFunctions}
+                    />
+                  case "Code":
+                    return <Code {...codeProps} />
+                  default:
+                    return (
+                      <div className="h-full p-2 flex items-center justify-center min-h-[150px] text-xs lowercase">
+                        <span className="text-[#D0D0D0]">No data found</span>
+                      </div>
+                    )
+                }
+              })()}
             </div>
           </div>
         </div>
       </div>
-
-      {/* Wallet Connection Modal */}
-      <WalletConnectModal
-        isOpen={isWalletModalOpen}
-        onClose={() => setIsWalletModalOpen(false)}
-      />
-    </>
+    </div>
   );
 }
