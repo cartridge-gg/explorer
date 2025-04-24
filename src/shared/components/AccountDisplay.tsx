f063e55a46349e89fa9e290546c4ef34011df3fb
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2025-04-25 00:38:28 +0900

    Show wallet icon and controller username (#62)

--- src/shared/components/AccountDisplay.tsx
+++ src/shared/components/AccountDisplay.tsx
@@ -1,14 +1,35 @@
 import { useAccount } from "@starknet-react/core";
 import { truncateString } from "@/shared/utils/string";
 import WalletIcon from "../icons/WalletIcon";
-import { useCallback, useMemo, useRef, useState, HTMLAttributes } from "react";
+import { useCallback, useMemo, useRef, useState, HTMLAttributes, useEffect } from "react";
 import WalletConnectModal from "./wallet_connect";
 import { AccountModal } from "./AccountModal";
 import { useCallCart } from "@/store/ShoppingCartProvider";
+import ControllerConnector from "@cartridge/connector/controller";
 
 export default function AccountDisplay(props: HTMLAttributes<HTMLDivElement>) {
   const { state } = useCallCart();
-  const { address, status } = useAccount();
+  const { address, status, connector } = useAccount();
+
+  const isController = useMemo(() => connector && "controller" in connector, [connector])
+  const [username, setUsername] = useState<string>();
+  useEffect(() => {
+    if (!isController) {
+      setUsername(undefined)
+      return
+    }
+    (connector as ControllerConnector).controller.username()?.then(setUsername)
+  }, [connector, isController])
+
+  const icon = useMemo(() => {
+    if (!connector?.icon) {
+      return
+    }
+    if (typeof connector?.icon === "string") {
+      return connector?.icon
+    }
+    return connector?.icon.light
+  }, [connector])
 
   const [isCartModalOpen, setIsCartModalOpen] = useState(false);
   const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
@@ -17,12 +38,12 @@ export default function AccountDisplay(props: HTMLAttributes<HTMLDivElement>) {
   const statusText = useMemo(
     () =>
       status === "connected" && address
-        ? truncateString(address, 3)
+        ? username ?? truncateString(address, 3)
         : status === "connecting"
-        ? "Connecting..."
-        : "Connect",
+          ? "Connecting..."
+          : "Connect",
 
-    [address, status]
+    [address, status, username]
   );
 
   const handleClick = useCallback(() => {
@@ -45,9 +66,8 @@ export default function AccountDisplay(props: HTMLAttributes<HTMLDivElement>) {
     }
   };
 
-  const classes = `relative bg-white hover:bg-primary hover:text-white hover:border-primary sm:w-[122px] border border-borderGray font-bold flex px-3 py-1 gap-3 items-center justify-between cursor-pointer ${
-    props.className || ""
-  } ${status === "connected" ? "" : "uppercase"} `;
+  const classes = `relative bg-white hover:bg-primary hover:text-white hover:border-primary sm:w-[122px] border border-borderGray font-bold flex px-3 py-1 gap-3 items-center justify-between cursor-pointer ${props.className || ""
+    } ${status === "connected" ? "" : "uppercase"} `;
 
   return (
     <>
@@ -58,7 +78,7 @@ export default function AccountDisplay(props: HTMLAttributes<HTMLDivElement>) {
         onMouseLeave={handleMouseLeave}
         className={classes}
       >
-        <WalletIcon />
+        {icon ? <img className="size-4" src={icon} alt="controller" /> : <WalletIcon />}
         <div className="max-sm:hidden">
           <span ref={textRef}>{statusText}</span>
           {status === "connected" && state.calls.length > 0 && (
