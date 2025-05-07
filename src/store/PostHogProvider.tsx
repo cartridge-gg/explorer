698a94a1e53cc8e1fc1f4ed83c5a104d19bcf673
Tarrence van As <tarrence@vanas.family>
2025-05-07 16:04:20 -0400

    Configure vercel ingest proxy

--- src/store/PostHogProvider.tsx
+++ src/store/PostHogProvider.tsx
@@ -1,15 +1,16 @@
 import { PropsWithChildren } from "react";
 import { PostHogContext, PostHogWrapper } from "@cartridge/utils";
 
-const posthog = new PostHogWrapper(
-  import.meta.env.VITE_POSTHOG_KEY!,
-  {
-    host: import.meta.env.VITE_POSTHOG_HOST,
-    autocapture: true,
-  },
-);
+const posthog = new PostHogWrapper(import.meta.env.VITE_POSTHOG_KEY!, {
+  host: import.meta.env.VITE_POSTHOG_HOST,
+  autocapture: true,
+});
 
 export function PostHogProvider({ children }: PropsWithChildren) {
+  if (!import.meta.env.VITE_POSTHOG_KEY) {
+    return <>{children}</>;
+  }
+
   return (
     <PostHogContext.Provider value={{ posthog }}>
       {children}
