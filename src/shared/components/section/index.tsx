6b8f449023cbf9a5f5d9087a6b0e38aa60421f63
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2025-05-10 00:28:33 +0900

    Add link to class hash on contract page (#93)

--- src/shared/components/section/index.tsx
+++ src/shared/components/section/index.tsx
@@ -1,17 +1,21 @@
+import { cn } from "@cartridge/ui-next";
+
 export interface SectionBoxEntryProps {
   title: string;
   children?: React.ReactNode;
   bold?: boolean;
+  className?: string;
 }
 
 export function SectionBoxEntry({
   title,
   children,
   bold = true,
+  className,
 }: SectionBoxEntryProps) {
   return (
     <div>
-      <div className={`${bold ? "font-bold" : ""} uppercase mb-[5px]`}>
+      <div className={cn(`uppercase mb-[5px]`, bold ? "font-bold" : undefined, className)}>
         {title}
       </div>
       <div>{children}</div>
