d8ab6ed9c0b9a3f05afadde273c9da012ea21c35
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-05-15 14:53:26 +0200

    Show only available wallets + controller

--- src/store/starknetProvider.tsx
+++ src/store/starknetProvider.tsx
@@ -6,38 +6,19 @@ import {
   jsonRpcProvider,
   useInjectedConnectors,
 } from "@starknet-react/core";
-import { Connector, InjectedConnector } from "@starknet-react/core";
+import { constants } from "starknet";
 import ControllerConnector from "@cartridge/connector/controller";
-import { WebWalletConnector } from "starknetkit/webwallet";
-import { CHAIN_ID, rpcUrl } from "@/constants/rpc";
+import { rpcUrl, CHAIN_ID } from "@/constants/rpc";
 
-const ENABLE_CONTROLLER =
-  window.ENABLE_CONTROLLER ?? import.meta.env.VITE_ENABLE_CONTROLLER === "true";
-
-export const cartridge_controller = ENABLE_CONTROLLER
-  ? new ControllerConnector({
-      chains: [
-        {
-          rpcUrl: rpcUrl(),
-        },
-      ],
-      defaultChainId: CHAIN_ID,
-      url: import.meta.env.VITE_KEYCHAIN_URL,
-    })
-  : null;
-
-const connectors = ENABLE_CONTROLLER
-  ? [cartridge_controller].filter(
-      (connector): connector is ControllerConnector => connector !== null,
-    )
-  : [];
-
-export const availableConnectors: Connector[] = [
-  ...connectors,
-  new InjectedConnector({ options: { id: "argentX", name: "Argent X" } }),
-  new InjectedConnector({ options: { id: "braavos", name: "Braavos" } }),
-  new WebWalletConnector({ url: "https://web.argent.xyz" }),
-];
+const controllerConnector = new ControllerConnector({
+  chains: [
+    {
+      rpcUrl: rpcUrl(),
+    },
+  ],
+  defaultChainId: CHAIN_ID,
+  url: import.meta.env.VITE_KEYCHAIN_URL,
+});
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
   const providers = jsonRpcProvider({
@@ -46,11 +27,17 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
     }),
   });
 
-  const { connectors } = useInjectedConnectors({});
+  const { connectors } = useInjectedConnectors({
+    recommended: [controllerConnector],
+  });
 
   return (
     <StarknetConfig
-      chains={[import.meta.env.VITE_IS_TESTNET === "true" ? sepolia : mainnet]}
+      chains={[
+        import.meta.env.VITE_CHAIN_ID === constants.StarknetChainId.SN_MAIN
+          ? mainnet
+          : sepolia,
+      ]}
       provider={providers}
       connectors={connectors}
     >
