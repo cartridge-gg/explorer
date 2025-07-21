301a156bff5cb0c583033fab8f44116d8564a94c
Nuriman Quddus <65181897+nrmnqdds@users.noreply.github.com>
2025-07-21 22:00:32 +0800

    refactor: contract function call logics (#193)

--- src/modules/Contract/page.tsx
+++ src/modules/Contract/page.tsx
@@ -196,15 +196,13 @@ export function Contract() {
     if (currentIndex === -1) return;
 
     switch (e.key) {
-      case "ArrowDown":
-      case "j": {
+      case "ArrowDown": {
         e.preventDefault();
         const newIndex = Math.min(filtered.length - 1, currentIndex + 1);
         setSelected(filtered[newIndex]);
         break;
       }
-      case "ArrowUp":
-      case "k": {
+      case "ArrowUp": {
         e.preventDefault();
         const newIndex = Math.max(0, currentIndex - 1);
         setSelected(filtered[newIndex]);
@@ -265,8 +263,8 @@ export function Contract() {
             parseResponse: false,
           });
           onUpdate(f.name, { result: result, error: undefined });
-        } else {
-          const result = await account!.execute([
+        } else if (account) {
+          const result = await account.execute([
             {
               calldata: calldata,
               entrypoint: f.name,
@@ -287,7 +285,7 @@ export function Contract() {
 
   const onAddToCart = useCallback(
     (f: FunctionWithType) => {
-      if (!contract || isReadFunction(f) || !account) {
+      if (!contract || isReadFunction(f)) {
         return;
       }
 
@@ -601,10 +599,7 @@ export function Contract() {
                             onChange={(i, value) =>
                               onChange(selected, i, value)
                             }
-                            disabled={
-                              !contract ||
-                              (!isReadFunction(selected) && !account)
-                            }
+                            disabled={!contract}
                           />
                         ) : (
                           <p className="h-full flex items-center justify-center text-foreground-300">
@@ -627,7 +622,6 @@ export function Contract() {
                                 <Button
                                   variant="secondary"
                                   onClick={() => onAddToCart(selected)}
-                                  disabled={!account}
                                   className="h-[30px] gap-[7px] px-[10px] py-[6px] normal-case font-sans bg-background-200 border border-[#454B46]"
                                 >
                                   <PlusIcon
@@ -642,9 +636,7 @@ export function Contract() {
                                 <Button
                                   variant="primary"
                                   className="h-[30px] px-[10px] py-[6px] bg-foreground-100 text-background-100"
-                                  disabled={
-                                    !account || form[selected.name]?.loading
-                                  }
+                                  disabled={form[selected.name]?.loading}
                                   onClick={() => onCallOrExecute(selected)}
                                 >
                                   <span className="text-[13px]/[16px] font-semibold uppercase">
@@ -833,10 +825,7 @@ export function Contract() {
                             onChange={(i, value) =>
                               onChange(selected, i, value)
                             }
-                            disabled={
-                              !contract ||
-                              (!isReadFunction(selected) && !account)
-                            }
+                            disabled={!contract}
                           />
                         ) : (
                           <div className="h-full flex items-center justify-center text-foreground-300">
@@ -859,7 +848,6 @@ export function Contract() {
                                 <Button
                                   variant="secondary"
                                   onClick={() => onAddToCart(selected)}
-                                  disabled={!account}
                                   className="h-[30px] gap-[7px] px-[10px] py-[6px] normal-case font-sans bg-background-200 border border-[#454B46]"
                                 >
                                   <PlusIcon
@@ -874,9 +862,7 @@ export function Contract() {
                                 <Button
                                   variant="primary"
                                   className="h-[30px] px-[10px] py-[6px] bg-foreground-100 text-background-100"
-                                  disabled={
-                                    !account || form[selected.name]?.loading
-                                  }
+                                  disabled={form[selected.name]?.loading}
                                   onClick={() => onCallOrExecute(selected)}
                                 >
                                   <span className="text-[13px]/[16px] font-semibold uppercase">
